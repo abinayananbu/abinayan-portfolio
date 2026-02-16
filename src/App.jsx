f76dfe418c8b50e33087a124c86c1a0a98612591
abinayananbu <abinayananbu@gmail.com>
2026-02-16 17:27:10 +0530

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
